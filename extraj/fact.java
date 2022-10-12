cfeccd603f5245d5654d144184114c02e5e81e3e
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-10-12 23:28:57 +0530

    Create fact.java

---
+++ extraj/fact.java
@@ -0,0 +1,17 @@
+package Java.kkrecur;
+
+public class fact {
+    public static void main(String[]args)
+ {
+      System.out.println(facto(5));  
+    }
+   public static int facto(int n)
+    {
+        if(n==1)
+        {
+            return 1;
+
+        }
+        return n * facto(n-1);
+    }
+}

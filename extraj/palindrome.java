e6eeb75228a6b24a59fa251ea3d2b24a36602996
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-10-14 23:32:44 +0530

    Create palindrome.java

---
+++ extraj/palindrome.java
@@ -0,0 +1,27 @@
+package Java.kkrecur;
+
+public class palindrom {
+    static int sum =0;
+    static boolean palin(int n)
+    {
+        return n == reverse(n);
+    }
+    static int reverse (int n)
+    {
+        int digits = (int)(Math.log10(n))+1;
+        return helper(n,digits);
+    }
+    private static int  helper (int n , int digits )
+    {
+      if(n%10==n)
+      {
+        return n;
+      }
+      int rem = n%10;
+      return rem * (int)(Math.pow(10,digits-1)) + helper(n/10, digits-1);
+    }
+  public static void main (String []args)
+  {
+    System.out.println(palin(1234321));
+  }
+}
